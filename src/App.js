cc0d22bb2dd31195d2727c0bf8dd3af50e444965
crvroberto <carlos010442@hotmail.com>
2021-06-27 15:34:27 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { CSVLink } from "react-csv";
+
 import './App.css';
 
+
 function App() {
+
+  const quaRifa = 300
+  const quaFolha = 10
+  const quadDivi = quaRifa / quaFolha
+
+  const array = []  
+  const novoArray = []
+  
+  for (let i = 1; i <= quaRifa; i++) {  
+      
+    let str = "" + i
+    let pad = "0000"
+   str = pad.substring(0, pad.length - str.length) + str
+    array.push([str])}
+
+
+  for (var i = 0; i < array.length; i = i + quadDivi) {
+    novoArray.push(array.slice(i, i + quadDivi))
+  }
+
+  
+  const arrayfinal = []
+  function chama (val){
+        novoArray.forEach((a)=>{
+          arrayfinal.push(a[val])
+          
+        })
+  }
+
+  for(let i = 0; i < quadDivi; i++){   chama(i)  }
+
+
+  arrayfinal.unshift(["0000"])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    
+  <div className="App">
+    <h1>Rifa</h1>
+      <CSVLink
+  data={arrayfinal}
+  filename={`Rifa${quaRifa}unidades.csv`}
+  className="btn btn-primary"
+  target="_blank"
+  separator={";"}
+  enclosingCharacter={``}
+>
+  Download me
+</CSVLink>
+   
     </div>
   );
 }
