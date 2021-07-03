f28a7c26e7480d6f0fc46d21cc1d3f30435fa68c
crvroberto <carlos010442@hotmail.com>
2021-07-03 10:34:44 -0300

    edição final

--- src/App.js
+++ src/App.js
@@ -2,44 +2,57 @@ import { CSVLink } from "react-csv";
 import './App.css';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import {TextField, Button , } from '@material-ui/core';
 import { useState } from "react";
 
 function App() {
 
   const [quaRifa, setQuarifa] = useState(100)
+  const [quaFolha, setQuafolha] = useState(10)
+  const [down, setDown] = useState([])
 
-  const quaFolha = 10
-  const quadDivi = quaRifa / quaFolha
-
-  const array = []  
+  
+  const arrayfinal =[]
+  const array = []
   const novoArray = []
-  
-  for (let i = 1; i <= quaRifa; i++) {  
-      
-    let str = "" + i
-    let pad = "0000"
-   str = pad.substring(0, pad.length - str.length) + str
-    array.push([str])}
+
+  function gerator() {
 
 
-  for (var i = 0; i < array.length; i = i + quadDivi) {
-    novoArray.push(array.slice(i, i + quadDivi))
+    for (let i = 1; i <= quaRifa; i++) {
+
+      let str = "" + i
+      let pad = "0000"
+      str = pad.substring(0, pad.length - str.length) + str
+      array.push([str])
+    }
+
+
+    for (var i = 0; i < array.length; i = i + quaRifa / quaFolha) {
+      novoArray.push(array.slice(i, i + quaRifa / quaFolha))
+    }
+
+
+
+    function chama(val) {
+      novoArray.forEach((a) => {
+        arrayfinal.push(a[val])
+
+      })
+    }
+
+    for (let i = 0; i < quaRifa / quaFolha; i++) { chama(i) }
+
+
+    arrayfinal.unshift(["0000"])
+    
+    setDown(arrayfinal)
+
+
+
   }
 
-  
-  const arrayfinal = []
-  function chama (val){
-        novoArray.forEach((a)=>{
-          arrayfinal.push(a[val])
-          
-        })
-  }
-
-  for(let i = 0; i < quadDivi; i++){   chama(i)  }
-
-
-  arrayfinal.unshift(["0000"])
+  console.log(down)
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -49,29 +62,37 @@ function App() {
       },
     },
   }))
-  
-    const classes = useStyles();
-  
-  return (
-    
-  <div className="App">
-    
-    <form className={classes.root} noValidate autoComplete="off">
-    <h1>Rifa</h1>
-    <TextField id="outlined-basic" label="Quantas Rifas?" variant="outlined" value={quaRifa} type="number" onChange={(e)=>{setQuarifa(e.target.value)}}/>
-    <TextField id="outlined-basic" label="Quantas por folha?" variant="outlined" />
-    
+
+  const classes = useStyles();
+
+  function Download(props) {
+    return (
       <CSVLink
-  data={arrayfinal}
-  filename={`Rifa${quaRifa}unidades.csv`}
-  className="btn btn-primary"
-  target="_blank"
-  separator={";"}
-  enclosingCharacter={``}
->
-  Download me
-</CSVLink>
-</form>
+        data={down}
+        filename={`Rifa${quaRifa}unidades.csv`}
+        className="btn btn-secondary"
+        target="_blank"
+        separator={";"}
+        enclosingCharacter={``}
+
+      >
+        <Button variant="contained" color="secondary">Download {down.length -1} unidades</Button>
+       
+      </CSVLink>)
+  }
+  return (
+
+    <div className="App">
+
+      <div className={classes.root} noValidate autoComplete="off">
+        <h1>Rifa</h1>
+        <TextField id="outlined-basic" label="Quantas Rifas?" variant="outlined" value={quaRifa} type="number" onChange={(e) => { setQuarifa(e.target.value) }} />
+        <TextField id="outlined-basic" label="Quantas por folha?" variant="outlined" value={quaFolha} type="number" onChange={(e) => { setQuafolha(e.target.value) }} />
+        <Button onClick={gerator} variant="contained" color="primary"> Gerar </Button >
+      
+      </div>
+      
+      {down.length > 1 ? <Download/> : <div></div>}
     </div>
   );
 }
