47dabda22afd33b03ef143289fafe36724b50404
crvroberto <carlos010442@hotmail.com>
2021-07-03 10:53:18 -0300

    edição final 2

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function App() {
     <div className="App">
 
       <div className={classes.root} noValidate autoComplete="off">
-        <h1>Rifa</h1>
+        <h1>Rifa - Copiadora Vieira</h1>
         <TextField id="outlined-basic" label="Quantas Rifas?" variant="outlined" value={quaRifa} type="number" onChange={(e) => { setQuarifa(e.target.value) }} />
         <TextField id="outlined-basic" label="Quantas por folha?" variant="outlined" value={quaFolha} type="number" onChange={(e) => { setQuafolha(e.target.value) }} />
         <Button onClick={gerator} variant="contained" color="primary"> Gerar </Button >
